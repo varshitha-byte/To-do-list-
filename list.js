581ee3de97145de468042614cefb3db704b04d04
neelimagundugari <gundugarineelima@gmail.com>
2025-12-20 00:17:19 +0530

    Functionality of list update

--- list.js
+++ list.js
@@ -1,33 +1,63 @@
 
-
 alert("Manage your tasks here!");
 
-const addBtn=document.querySelector(".button");
-const  taskInput=document.querySelector(".box");
+const addBtn = document.querySelector(".button");
+const taskInput = document.querySelector(".box");
 const todoList = document.querySelector(".taskList");
 
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
 
-function addTask(){
-     const taskText=taskInput.value.trim();
+  const li = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const icon = document.createElement("i"); 
 
-    if(taskText=== "") return;
+  checkbox.type = "checkbox";
+  icon.className = "fa-solid fa-trash";
 
-    const li=document.createElement("li");
-    li.classList.add("todo-item");
-    li.textContent=taskText;
+  label.appendChild(checkbox);
+  label.append(" " + taskText);
 
-    todoList.appendChild(li);
+  li.appendChild(label);
+  li.appendChild(icon);
+  todoList.appendChild(li);
 
-    taskInput.value= "";
+  taskInput.value = "";
+
+  
+
+  // click anywhere on li
+  li.addEventListener("click", () => {
+    checkbox.checked = !checkbox.checked;
+    li.classList.toggle("completed", checkbox.checked);
+    li.style.backgroundColor = "#E6FFE6";
+
+    li.style.color = "#00A300";
+
+  });
+
+  li.addEventListener("click",()=>{
+      if(!checkbox.checked){
+        li.style.backgroundColor = "";
+
+        li.style.color = "";
+      }
+  });
 
 
+   
 
+  // prevent double toggle
+  checkbox.addEventListener("click", (e) => e.stopPropagation());
+
+  // delete task
+ 
 }
 
 addBtn.addEventListener("click", addTask);
 
-taskInput.addEventListener("keypress", (e)=>{
-    if(e.key==="Enter"){
-    addTask();
-}
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") addTask();
 });
